e8d480d3577edd345dcf71183a303658d30c6376
Papós Ádám <paposadam@gmail.com>
2023-07-12 19:02:38 +0200

    Nested JSON parsing

--- endpoint.py
+++ endpoint.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 import json
 
-def process_json(input_dict):
+def process_json2(input_dict):
 	print("-----------------------------" + str(type(input_dict)))
 	print("________________" + input_dict["resourceType"])
 	for entry in input_dict["entry"]:
@@ -13,6 +13,12 @@ def process_json(input_dict):
 			#for subdetail in detail["id"]:
 			#	print(subdetail)
 		input()
+
+###working
+def process_json(input_dict):
+	print("-----------------------------" + str(type(input_dict)))
+	print("________________" + input_dict["resourceType"])
+	print("::::::::::::::::::::" + input_dict["entry"][0]["resource"]["id"] + "::::::::::::::::::::")
     
 
 
