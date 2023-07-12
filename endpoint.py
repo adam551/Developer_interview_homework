6813fb2cf9e04c6b979baacb0cf259edf942c787
Papós Ádám <paposadam@gmail.com>
2023-07-12 17:14:02 +0200

    Add endpoint functionality

--- endpoint.py
+++ endpoint.py
@@ -1,2 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import json
+import os
+
+def process_json(input_string):
+	print("-----------------------------" + str(type(input_string)))
+	print("________________" + input_string["resourceType"])
+    
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def upload_json():
+    data = process_json(request.get_json())
+    return "data"
+     
+
+if __name__ == '__main__':
+    app.run()
+    
