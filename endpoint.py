f9061b5145c726b4316e31cbc2814636eba31fb4
Papós Ádám <paposadam@gmail.com>
2023-07-12 18:54:14 +0200

    JSON parsing tests

--- endpoint.py
+++ endpoint.py
@@ -1,12 +1,20 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 import json
-import os
 
-def process_json(input_string):
-	print("-----------------------------" + str(type(input_string)))
-	print("________________" + input_string["resourceType"])
+def process_json(input_dict):
+	print("-----------------------------" + str(type(input_dict)))
+	print("________________" + input_dict["resourceType"])
+	for entry in input_dict["entry"]:
+		print(str(type(entry)))
+		for detail in entry["resource"]:
+			print(str(type(detail)) + "\n")
+			#print(detail["id"])
+			#for subdetail in detail["id"]:
+			#	print(subdetail)
+		input()
     
+
 
 app = Flask(__name__)
 
