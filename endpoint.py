c892bae8041d9e4ff2d834178856926f07d99156
Papós Ádám <paposadam@gmail.com>
2023-07-13 16:17:06 +0200

    JSON processing and output implementation

--- endpoint.py
+++ endpoint.py
@@ -1,33 +1,66 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from datetime import date
 import json
 
-def process_json2(input_dict):
-	print("-----------------------------" + str(type(input_dict)))
-	print("________________" + input_dict["resourceType"])
-	for entry in input_dict["entry"]:
-		print(str(type(entry)))
-		for detail in entry["resource"]:
-			print(str(type(detail)) + "\n")
-			#print(detail["id"])
-			#for subdetail in detail["id"]:
-			#	print(subdetail)
-		input()
-
-###working
 def process_json(input_dict):
-	print("-----------------------------" + str(type(input_dict)))
-	print("________________" + input_dict["resourceType"])
-	print("::::::::::::::::::::" + input_dict["entry"][0]["resource"]["id"] + "::::::::::::::::::::")
-    
+	json_list = []
+	for i in range(len(input_dict['entry'])):
+		code_list = []
+		measurement_value_list = []
+		measurement_unit_list = []
+		data = {}
+		try:
+			data['observationId'] = input_dict['entry'][i]['resource']['id']
+		except:
+			data['observationId'] = "ObservationIdNotFound"
+		try:
+			patientId = str(input_dict['entry'][i]['resource']['subject']['reference']).split('/')[1]
+		except:
+			patientId = "PatientIdNotFound"
+		data['patientId'] = patientId
+		try:
+			performerId = str(input_dict['entry'][i]['resource']['performer'][0]['reference']).split('/')[1]
+		except:
+			performerId = "PerformerNotFound"
+		data['performerId'] = performerId
+
+		try:
+			for j in range(len(input_dict['entry'][i]['resource']['code']['coding'])):
+				if input_dict['entry'][i]['resource']['code']['coding'][j]['system'].find("http://loinc.org") >= 0:
+					code_list.append(input_dict['entry'][i]['resource']['code']['coding'][j])
+			data['measuerementCoding'] = code_list
+		except:
+			data['measuerementCoding'] = "MeasurementCodingNotFound"
+
+		try:
+			for j in range(len(input_dict['entry'][i]['resource']['component'])):
+				for k in range(len(input_dict['entry'][i]['resource']['component'][j]['code']['coding'])):
+					if input_dict['entry'][i]['resource']['component'][j]['code']['coding'][0]['system'].find("http://loinc.org") >= 0:
+						for l in range(len(input_dict['entry'][i]['resource']['component'])):
+							measurement_value_list.append(input_dict['entry'][i]['resource']['component'][l]['valueQuantity']['value'])
+							measurement_unit_list.append(input_dict['entry'][i]['resource']['component'][l]['valueQuantity']['unit'])
+			data['measurementValue'] = measurement_value_list
+			data['measurementUnit'] = measurement_unit_list
+		except:
+			data['measurementValue'] = "ValueNotFound"
+			data['measurementUnit'] = "UnitNotFound"
+
+		try:
+			data['measurementDate'] = str(input_dict['entry'][i]['resource']['effectiveDateTime']).split('T')[0]
+		except:
+			data['measurementDate'] = "DateNotFound"
+		data['dataFetched'] = str(date.today())
+		json_list.append(data)
+
+	output_dict = json.dumps(json_list)
+	return output_dict
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def upload_json():
-    data = process_json(request.get_json())
-    return "data"
+    return process_json(request.get_json())
      
 
 if __name__ == '__main__':
